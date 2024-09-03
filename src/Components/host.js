c08365056871140e47a77bee2d4c7402a144f26f
mdrabil <ermdrabil@gmail.com>
2024-09-03 23:15:38 +0530

    addlink

--- src/Components/host.js
+++ src/Components/host.js
@@ -1,3 +1,3 @@
-// export const host= 'https://makbul.vercel.app'
-export const host = 'http://localhost:8080'
+export const host= 'https://makbul.vercel.app'
+// export const host = 'http://localhost:8080'
 
